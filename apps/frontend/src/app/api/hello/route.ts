13f2ff176a4dfd7bf78373838b216afa88a8ef9a
aatiphfaraz <aatiphfaraz@gmail.com>
2025-05-20 20:57:38 +0530

    flow completed

--- apps/frontend/src/app/api/hello/route.ts
+++ apps/frontend/src/app/api/hello/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
 // GET /api/hello
-export async function GET() {
+export async function POST() {
   return NextResponse.json({ message: 'Hello, world!' });
 }
